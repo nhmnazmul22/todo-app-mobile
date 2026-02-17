c1e12f525371b27147c3ee3927d7aef8bbcbda9d
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-02-17 22:29:51 +0600

    Learn little things

---
+++ app/about.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from "react-native";
+
+const AboutScreen  = ()=>{
+    return (
+        <View>
+            <Text>This is About page</Text>
+        </View>
+    )
+}
+
+export default AboutScreen
